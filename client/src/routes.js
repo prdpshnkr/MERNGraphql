ea4741eb2eeda87f2e3ce8e605a980a19129c455
Pradeep S <prdpfotos@gmail.com>
2022-02-09 07:44:33 +0530

    client setup

---
+++ client/src/routes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Routes extends Component {
+    render(){
+        return (
+            <>
+            routes</>
+        )
+    }
+}
+
+export default Routes
